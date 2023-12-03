eca21dc3c0ebb47ee9fffec0e54872e2fe1e03b5
dkavla <dkowalczyk135@gmail.com>
2023-12-02 20:32:40 -0500

    Ensure home tab loads when first loading page

--- src/index.js
+++ src/index.js
@@ -1,74 +1,24 @@
 import './home.css';
 import dish from './french-dish.jpeg';
+import { setHome } from './home';
+import { setMenu } from './menu';
 
-// holds the main content
-const mainDiv = document.getElementById('content');
+setHome();
 
-/* Header Section */
-// header
-const header = document.createElement('header');
-const nameDiv = document.createElement('div');
-nameDiv.id = 'name';
-nameDiv.innerHTML = "La Grande Culinaire"
+// const home = document.getElementById('home');
+// const menu = document.getElementById('menu');
+// const contact = document.getElementById('contact');
 
-// navbar sections
-const navBar = document.createElement('nav');
+// home.addEventListener('click', () => {
+//     setHome();
+//     console.log('Home');
+// });
 
-const homeBtn = document.createElement('div');
-homeBtn.id = 'home';
-homeBtn.innerHTML = "Home";
-homeBtn.addEventListener('click', () => {
-    console.log('Home');
-});
+// menu.addEventListener('click', () => {
+//     setMenu();
+//     console.log("Menu");
+// });
 
-const menuBtn = document.createElement('div');
-menuBtn.id = 'menu';
-menuBtn.innerHTML = "Menu";
-menuBtn.addEventListener('click', () => {
-    console.log('Menu');
-});
-
-const contactBtn = document.createElement('div');
-contactBtn.id = 'contact';
-contactBtn.innerHTML = "Contact Us";
-contactBtn.addEventListener('click', () => {
-    console.log('Contact');
-});
-
-navBar.appendChild(homeBtn);
-navBar.appendChild(menuBtn);
-navBar.appendChild(contactBtn);
-header.appendChild(nameDiv);
-header.appendChild(navBar);
-
-/* Main Section */
-const main = document.createElement('main');
-
-const specializationTitle = document.createElement('div');
-specializationTitle.id = 'center-main';
-specializationTitle.innerHTML = "Authentic French Cuisine"
-
-const descriptionTitle = document.createElement('div');
-descriptionTitle.id = 'description-title';
-descriptionTitle.innerHTML = "What We Bring To the Table";
-
-const description = document.createElement('p');
-description.id = 'description';
-description.innerHTML = "La Grande Culinaire is one of the top French dining spots in the heart of New York City. Our chefs are world class trained by top chefs, such as Marco Pierre White, Raymond Blanc, Gordon Ramsay, and more. They strive to bring cuisine that satisfies and quells hunger. We believe your dish should be both an art and pleasure."
-
-const servingHours = document.createElement('p');
-servingHours.id = 'serving-hours';
-servingHours.innerHTML = "We serve breakfast from 8:00 AM - 11:00 AM, lunch 11:00 AM - 4:00 PM, late lunch 4:00 PM - 4:45 PM, dinner 5:00 PM - 12:00 AM and brunch 9:00 AM - 3:45 PM. Reservations are recommended."
-
-main.appendChild(specializationTitle);
-main.appendChild(descriptionTitle);
-main.appendChild(description);
-main.appendChild(servingHours);
-
-// add components content div
-// header section
-mainDiv.appendChild(header);
-
-
-// main section
-mainDiv.appendChild(main);
+// contact.addEventListener('click', () => {
+//     console.log('Contact');
+// });
