2e248586f6290e4b9bb31ea8ea1826bc7a5edf23
dkavla <dkowalczyk135@gmail.com>
2023-12-02 19:17:33 -0500

    Edit descriptions for main's paragraph tags

--- src/index.js
+++ src/index.js
@@ -49,12 +49,15 @@ specializationTitle.id = 'center-main';
 specializationTitle.innerHTML = "Authentic French Cuisine"
 
 const descriptionTitle = document.createElement('div');
+descriptionTitle.id = 'description-title';
 descriptionTitle.innerHTML = "What We Bring To the Table";
 
 const description = document.createElement('p');
-description.innerHTML = "La Grande Culinaire aims to be the top French dining spot in the heart of New York City. Our chefs are world class trained by top artists like Marco Pierre White, Raymond Blanc, Gordon Ramsay, and others. They strive to bring cuisine that satisfies and quells hunger."
+description.id = 'description';
+description.innerHTML = "La Grande Culinaire is one of the top French dining spots in the heart of New York City. Our chefs are world class trained by top chefs, such as Marco Pierre White, Raymond Blanc, Gordon Ramsay, and more. They strive to bring cuisine that satisfies and quells hunger. We believe your dish should be both an art and pleasure."
 
 const servingHours = document.createElement('p');
+servingHours.id = 'serving-hours';
 servingHours.innerHTML = "We serve breakfast from 8:00 AM - 11:00 AM, lunch 11:00 AM - 4:00 PM, late lunch 4:00 PM - 4:45 PM, dinner 5:00 PM - 12:00 AM and brunch 9:00 AM - 3:45 PM. Reservations are recommended."
 
 main.appendChild(specializationTitle);
